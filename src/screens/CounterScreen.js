b4c0de2953f912da9ecdd99dc24f42596e2520f3
mutsutomi <m2.tmk@ezweb.ne.jp>
2021-03-09 12:59:36 +0900

    by61

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,18 +1,45 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 import {View, Text, StyleSheet, Button} from "react-native";
 
-const CounterScreen=()=>{
-  const [counter, setCounter]=useState(0);
+const reducer=(state, action)=>{
+  //state==={count:number}
+  //action==={type: "increment" || "decrement", payload: 1}
+
+  switch(action.type){
+    case "increment":
+      return {...state, count: state.count+action.payload};
+    case "decrement":
+      return {...state, count: state.count-action.payload};
+    case "reset":
+      return {...state, count: 0};
+    default:
+      return state;
+  };
+};
+
+const CounterScreen=()=>{ 
+  const [state, dispatch]=useReducer(reducer, {count: 0})
+  
   return <View>
     <Button
       title="increase"
-      onPress={()=>{setCounter(counter+1);}}
+      onPress={()=>{
+        dispatch({type: "increment", payload: 1})
+      }}
     />
     <Button
       title="decrease"
-      onPress={()=>{setCounter(counter-1);}}
+      onPress={()=>{
+        dispatch({type: "decrement", payload: 1})
+      }}
     />
-    <Text>Current Count;{counter}</Text>
+    <Button
+      title="reset"
+      onPress={()=>{
+        dispatch({type: "reset"})
+      }}
+    />
+    <Text>Current Count;{state.count}</Text>
   </View>
 };
 
